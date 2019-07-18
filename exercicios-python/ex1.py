634d2c32581b0d6abd95f7ecdceba6b77b5cd1e6
Obede <obede.silva7@gmail.com>
2019-07-17 23:51:37 -0300

    ex1

---
+++ exercicios-python/ex1.py
@@ -0,0 +1,5 @@
+nome = input ('Digite o nome do funcionário: ')
+horas_trabalho = int(input('Digite o número de horas trabalhadas: '))
+valor_hora = float(input('Digite o valor por hora: '))
+
+print(nome+' seu salário é R$'+str(horas_trabalho*valor_hora))
